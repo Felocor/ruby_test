cc0f2f6d8db801d2ac496f77926890b48745da51
mendoza <lfe.cortes@gmail.com>
2019-04-16 21:52:37 -0300

    Add class file types

---
+++ programming_basics/class.rb
@@ -0,0 +1,20 @@
+puts "String >> Ju Doggy"
+p "Ju Doggy".class
+puts
+puts "Integer >> 12"
+p 12.class
+puts
+puts "float >> 3.14"
+p 3.13.class
+puts
+puts "Array >> ['Dog', 2, 'leaf']"
+p ["Dog", 2, "leaf"].class
+puts
+puts "TrueClass >> true"
+p true.class
+puts
+puts "FalseClass >> false"
+p false.class
+puts
+puts "range >> (1..10)"
+p (1..10).class
